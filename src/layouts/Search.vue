b4e1bf51bbce2b53c86608f884cfa3ff72a3d984
hendriyono <hendriyono97@gmail.com>
2020-02-15 23:39:38 +0800

    initial commit

---
+++ src/layouts/Search.vue
@@ -0,0 +1,63 @@
+<template>
+  <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
+
+    <!-- (Optional) The Header -->
+    <q-header elevated class="row justify-center">
+      <q-toolbar class="col">
+        <q-toolbar-title>
+          Breaking break API example
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs>
+        <q-route-tab
+          :to="{ name: 'profile', params: { id: 1} }"
+          replace
+          label="Walter White"
+        />
+
+        <q-route-tab
+          :to="{ name: 'profile', params: { id: 2} }"
+          replace
+          label="Jesse Pinkman"
+        />
+
+         <q-route-tab
+          :to="{ name: 'profile', params: { id: 'random'} }"
+          replace
+          label="Random"
+        />
+      </q-tabs>
+    </q-header>
+
+    <!-- (Optional) The Footer -->
+    <q-footer>
+      <q-toolbar>
+        <q-toolbar-title>
+          Made with Love by <b>Hendriyono</b>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+    <q-page-container>
+      <!-- This is where pages get injected -->
+      <router-view :key="$route.params.id"/>
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  // name: 'LayoutName',
+  meta:{
+     title: 'Breaking Bad App'
+  },
+  data () {
+    return {
+      leftDrawer: true
+    }
+  }
+}
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <div class="q-pa-md q-gutter-md">
+            <div>
+            <q-chip icon="event">Add to calendar</q-chip>
+            <q-chip icon="bookmark">Bookmark</q-chip>
+            <q-chip icon="alarm" label="Set alarm" />
+            <q-chip class="glossy" icon="directions">Get directions</q-chip>
+            </div>
+            <div>
+            <q-chip color="primary" text-color="white" icon="event">
+                Add to calendar
+            </q-chip>
+            <q-chip color="teal" text-color="white" icon="bookmark">
+                Bookmark
+            </q-chip>
+            <q-chip class="glossy" color="orange" text-color="white" icon-right="star">
+                Star
+            </q-chip>
+            <q-chip color="red" text-color="white" icon="alarm" label="Set alarm" />
+            <q-chip color="deep-orange" text-color="white" icon="directions">
+                Get directions
+            </q-chip>
+            <q-chip>
+                <q-avatar icon="bookmark" color="red" text-color="white" />
+                Bookmark
+            </q-chip>
+            <q-chip>
+                <q-avatar color="red" text-color="white">50</q-avatar>
+                Emails
+            </q-chip>
+            <q-chip>
+                <q-avatar>
+                <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+                </q-avatar>
+                John
+            </q-chip>
+            </div>
+        </div>
+
+        <h3>Hello from home page</h3>
+        
+        <div class="q-pa-md q-gutter-sm">
+            <q-btn color="primary" label="Primary" />
+        </div>
+
+        <q-banner class="bg-primary text-white">
+            Unfortunately, the credit card did not go through, please try again.
+            <template v-slot:action>
+                <q-btn flat color="white" label="Dismiss" />
+                <q-btn flat color="white" label="Update Credit Card" />
+            </template>
+        </q-banner>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
